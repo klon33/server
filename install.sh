976feaa26bc51461ef05851ea5280742453bd6be
klon33 <33056126+klon33@users.noreply.github.com>
2024-08-12 14:22:20 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,31 @@
-apt-get update
-apt-get upgrade
+#!/bin/bash
 
-#install nginx-ui
+# Проверка на root-права
+if [ "$EUID" -ne 0 ]; then
+  echo "Пожалуйста, запустите скрипт с правами суперпользователя."
+  exit 1
+fi
+
+# Обновление списка пакетов и обновление установленных пакетов
+apt-get update -y
+apt-get upgrade -y
+
+# Установка curl, если не установлен
+if ! command -v curl &> /dev/null; then
+    echo "curl не установлен, устанавливаем..."
+    apt-get install curl -y
+fi
+
+# Установка nginx-ui
+echo "Устанавливаем nginx-ui..."
 bash <(curl -L -s https://raw.githubusercontent.com/0xJacky/nginx-ui/master/install.sh) install
 
-#install webmin
+# Установка webmin
+echo "Устанавливаем webmin..."
 curl -o setup-repos.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh
 sudo sh setup-repos.sh
+
+# Очистка
+rm -f setup-repos.sh
+
+echo "Установка завершена!"
