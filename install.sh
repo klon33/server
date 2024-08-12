34f116edf56067c37627b5f485d7d346e5b8a691
klon33 <33056126+klon33@users.noreply.github.com>
2024-08-12 14:20:30 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@ apt-get update
 apt-get upgrade
 
 bash <(curl -L -s https://raw.githubusercontent.com/0xJacky/nginx-ui/master/install.sh) install
+
+curl -o setup-repos.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh
+sudo sh setup-repos.sh
