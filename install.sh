f0e0a16a83c1e3c70cc4092d32599c7e4b4f3817
klon33 <33056126+klon33@users.noreply.github.com>
2024-08-12 14:20:58 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 apt-get update
 apt-get upgrade
 
+#install nginx-ui
 bash <(curl -L -s https://raw.githubusercontent.com/0xJacky/nginx-ui/master/install.sh) install
 
+#install webmin
 curl -o setup-repos.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh
 sudo sh setup-repos.sh
