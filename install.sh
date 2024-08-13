0e59172de008d8a13f78b9f1040861abab02cd60
klon33 <33056126+klon33@users.noreply.github.com>
2024-08-13 18:52:21 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 # Установка программ и первичная настройка ubuntu 24
 #https://github.com/GFW4Fun/x-ui-pro
 #установка vpn  и сертификата: sudo su -c "sudo su -c "bash <(wget -qO- https://raw.githubusercontent.com/GFW4Fun/x-ui-pro/master/x-ui-pro.sh) -install yes -ONLY_CF_IP_ALLOW yes"
+#https://github.com/MHSanaei/3x-ui
 
 
 # Проверка на root-права
